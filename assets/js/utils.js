25ebfbd100006c4ce037e8d868ff8fdc7f1f59ac
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-04-27 07:21:38 -0500

    noc

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -6,3 +6,26 @@ const traerTienda = async () => {
 };
 
 traerTienda();
+
+class Producto {
+    imagen = ""
+    title = ""
+
+
+    constructor(image, title) {
+        this.imagen = image;
+        this.title = title;
+    }
+
+    render() {
+        const div = document.createElement('div')
+        const jpg = document.createElement('img')
+        const titulo = document.createElement('p')
+        jpg.src = this.imagen;
+        titulo.innerHTML = this.title;
+
+        div.appendChild(jpg);
+        div.appendChild(titulo);
+        return div;
+    }
+}
