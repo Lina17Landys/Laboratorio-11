837421e7f5ad4970d27fb55644385312f1d7d3e3
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-04-30 10:31:47 -0500

    cambios

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,7 +1,14 @@
 import {getProducts} from "./utils";
 
-const render = async () => {
-    alert(window.location);
-}
+const urlParams = new URLSearchParams(window.location.search);
+const nombreProducto = urlParams.get('title');
+const ratingProducto = urlParams.get('rating');
+const precioProducto = urlParams.get('price');
+const desProducto = urlParams.get('description');
+const cateProducto = urlParams.get('category');
+const image = urlParams.get('image');
 
-render();
+document.getElementById('nombreProducto').innerText = nombreProducto;
+document.getElementById('precioProducto').innerText = `$${precioProducto}`;
+document.getElementById('desProducto').innerText = desProducto;
+document.getElementById('cateProducto').innerText = cateProducto;
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,34 +1,41 @@
-const traerTienda = async () => {
-    console.log("Tienda");
-    const response = await fetch("https://fakestoreapi.com/products");
-    const data = await response.json();
-    return data;
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+    let html = '';
+    data.forEach(product => {
+      html += `
+        <div class="product-container">
+          <a class="product-link">
+            <img src="${product.image}" alt="${product.title}" />
+            <h2>${product.title}</h2>
+            <p>$${product.price}</p>
+          </a>
+        </div>
+      `;
+    });
+    document.getElementById('container').innerHTML = html;
+
+    const productLinks = document.querySelectorAll('.product-link');
+    productLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        renderProduct(data.find(product => product.title === link.querySelector('h2').innerText));
+      });
+    });
+  })
+  .catch(error => console.error(error));
+
+const renderProduct = (product) => {
+  const container = document.getElementById('container');
+  container.innerHTML = `
+    <div class="container-1">
+      <div class="info">
+        <h1 class="product" id="nombreProducto">${product.title}</h1>
+        <p class="price" id="precioProducto">$${product.price}</p>
+        <p class="description" id="desProducto">${product.description}</p>
+        <p class="category" id="cateProducto">${product.category}</p>
+        <button>Buy now</button>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  `;
 };
-
-traerTienda();
-
-class Product {
-    #id = "";
-    #image = "";
-
-
-    constructor(id, image) {
-        this.#id = id;
-        this.#image = image;
-    }
-
-    render() {
-        const image = document.createElement("img");
-        image.id = "image" + this.#id;
-        image.src = this.#image;
-        image.classList.add("image")
-        return image;
-    }
-
-    addClickListener() {
-        const image = document.querySelector("#image" + this.#id);
-        image.addEventListener("click", () => {
-            window.location = "/product.html?productId=" + this.#id;
-        })
-    }
-}
